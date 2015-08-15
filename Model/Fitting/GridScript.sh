363d5921f63e4d11192b867ce55ff78bc59378ee
Adam Morris <amm4@cs.brown.edu>
2015-08-15 17:46:22 -0400

    Completed v2 of model fitting to simulated data.

--- Model/Fitting/GridScript.sh
+++ Model/Fitting/GridScript.sh
@@ -1,3 +1,3 @@
 #!/usr/local/bin/bash
 
-/gpfs/main/sys/shared/psfu/local/projects/matlab/R2013b/bin/matlab -nodisplay -nosplash -nodesktop -r "addpath '/home/amm4/git/DDE/Model/Fitting'; Fit('/home/amm4/git/DDE/Model/Fitting/v1/SimData.mat','/home/amm4/git/DDE/Model/Fitting/board_daw_fit.mat','/home/amm4/git/DDE/Model/Fitting/v1/SubjFits',25,$SGE_TASK_ID);exit;"
+/gpfs/main/sys/shared/psfu/local/projects/matlab/R2013b/bin/matlab -nodisplay -nosplash -nodesktop -r "addpath '/home/amm4/git/DDE/Model/Fitting'; Fit('/home/amm4/git/DDE/Model/Fitting/v2/SimData.mat','/home/amm4/git/DDE/Model/Fitting/board_daw_fit.mat','/home/amm4/git/DDE/Model/Fitting/v2/SubjFits',25,$SGE_TASK_ID);exit;"
