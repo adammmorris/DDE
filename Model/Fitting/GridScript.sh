e5d45ee35afe59477db8085f88c1c720788eae14
Adam Morris <amm4@cs.brown.edu>
2015-08-22 18:16:55 -0400

    Updated model fitting to dawstage2 data.

--- Model/Fitting/GridScript.sh
+++ Model/Fitting/GridScript.sh
@@ -1,3 +1,3 @@
 #!/usr/local/bin/bash
 
-/gpfs/main/sys/shared/psfu/local/projects/matlab/R2013b/bin/matlab -nodisplay -nosplash -nodesktop -r "addpath '/home/amm4/git/DDE/Model/Fitting'; Fit('/home/amm4/git/DDE/Model/Fitting/v4/NGLagents/SimData.mat','/home/amm4/git/DDE/Model/Fitting/board_daw_fit.mat','/home/amm4/git/DDE/Model/Fitting/v4/NGLagents/SubjFits',25,0,$SGE_TASK_ID);exit;"
+/gpfs/main/sys/shared/psfu/local/projects/matlab/R2013b/bin/matlab -nodisplay -nosplash -nodesktop -r "addpath '/home/amm4/git/DDE/Model/Fitting'; Fit_null('/home/amm4/git/DDE/Model/Fitting/v6/NGLagents/SimData.mat','/home/amm4/git/DDE/Model/Fitting/board_daw_fit.mat','/home/amm4/git/DDE/Model/Fitting/v6/NGLagents/SubjFits_null',25,0,$SGE_TASK_ID);exit;"
